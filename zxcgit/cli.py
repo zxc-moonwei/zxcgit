b747135e5f38f63aa1ac0da96aff5fa71c6c4705
zxc <1821326349@qq.com>
2023-05-02 20:32:41 +0800

    增加zxcgit init参数解析

--- zxcgit/cli.py
+++ zxcgit/cli.py
@@ -1,3 +1,20 @@
-# 4.28
+import argparse
 def main():
-    print("Hello,zxcgit2")
+    args = parse_arg()
+    args.func(args)
+def parse_arg():
+    # 添加命令解释器
+    parser = argparse.ArgumentParser()
+    # 添加子命令解释器
+    commands = parser.add_subparsers(dest='commands')
+    # 必须要输入子命令才能执行
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func = init)
+
+    return parser.parse_args()
+
+def init(args):
+    print("Hello,zxcgit")
+
