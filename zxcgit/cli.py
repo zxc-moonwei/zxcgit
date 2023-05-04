72505b7162f8f25ef9855d0ef48e9db33d8f9df6
zxc <1821326349@qq.com>
2023-05-04 12:22:12 +0800

    fix .git path

--- zxcgit/cli.py
+++ zxcgit/cli.py
@@ -1,9 +1,13 @@
 import argparse
 from . import data
 import os
+
+
 def main():
     args = parse_arg()
     args.func(args)
+
+
 def parse_arg():
     # 添加命令解释器
     parser = argparse.ArgumentParser()
@@ -13,11 +17,12 @@ def parse_arg():
     commands.required = True
 
     init_parser = commands.add_parser('init')
-    init_parser.set_defaults(func = init)
+    init_parser.set_defaults(func=init)
 
     return parser.parse_args()
 
+
 def init(args):
     data.init()
-    print(f"Successfully Initialized Empty Git Repository in {os.getcwd()}/{data.GIT_DIR}")
-
+    print('Successfully Initialized Empty Git Repository in %s' % os.path.join(os.getcwd(), data.GIT_DIR))
+    # print(f"Successfully Initialized Empty Git Repository in {os.getcwd()}/{data.GIT_DIR}")
