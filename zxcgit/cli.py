2d009b38b37e1c94d55cc155ca359b9fdce9d995
zxc <1821326349@qq.com>
2023-05-04 14:33:40 +0800

    完成hash-object命令

--- zxcgit/cli.py
+++ zxcgit/cli.py
@@ -15,9 +15,14 @@ def parse_arg():
     commands = parser.add_subparsers(dest='commands')
     # 必须要输入子命令才能执行
     commands.required = True
-
+    # 添加init命令
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
+
+    # 添加hash_object命令
+    hash_object_parser = commands.add_parser('hash_object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
     return parser.parse_args()
 
@@ -26,3 +31,8 @@ def init(args):
     data.init()
     print('Successfully Initialized Empty Git Repository in %s' % os.path.join(os.getcwd(), data.GIT_DIR))
     # print(f"Successfully Initialized Empty Git Repository in {os.getcwd()}/{data.GIT_DIR}")
+
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
--- zxcgit/data.py
+++ zxcgit/data.py
@@ -1,8 +1,22 @@
 # 管理.zxcgit 目录里面的数据
 
 import os
+import hashlib
 
 GIT_DIR = '.zxcgit'
 
+
 def init():
-    os.makedirs(GIT_DIR)
+    os.makedirs(GIT_DIR, exist_ok=True)
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()  # 转16进制
+    path = os.path.join(GIT_DIR, "objects", oid)
+    # os.path.dirname用于返回文件在的目录，如果目录已经存在，
+    # 由于exist_ok参数设置为True，不会引发错误。
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "wb") as out:
+        out.write(data)
+    return oid
+
